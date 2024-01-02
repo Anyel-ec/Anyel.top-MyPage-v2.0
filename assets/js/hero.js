1de5535cfa6b8a80b6d4f275f975f52ed99ab54a
anyel-ec <paulangel826@gmail.com>
2024-01-02 11:00:27 -0500

    add files of project Anyel EC (my page) upload

---
+++ assets/js/hero.js
@@ -0,0 +1,35 @@
+const phrases = ["Ciberseguridad", "Microservicios", "Soluciones en la Nube", "Análisis de Datos", "Desarrollo Full Stack"];
+let currentPhraseIndex = 0;
+let currentCharIndex = 0;
+const typedOutput = document.getElementById("typed-output");
+
+function typePhrase() {
+    const currentPhrase = phrases[currentPhraseIndex];
+    const currentChar = currentPhrase.charAt(currentCharIndex);
+    typedOutput.textContent += currentChar;
+    currentCharIndex++;
+
+    if (currentCharIndex < currentPhrase.length) {
+        setTimeout(typePhrase, 100); // Velocidad de escritura (ajustable)
+    } else {
+        setTimeout(erasePhrase, 100); // Tiempo antes de borrar (ajustable)
+    }
+}
+
+function erasePhrase() {
+    setTimeout(() => {
+        const currentPhrase = phrases[currentPhraseIndex];
+        typedOutput.textContent = currentPhrase.substring(0, currentCharIndex);
+        currentCharIndex--;
+
+        if (currentCharIndex >= 0) {
+            erasePhrase();
+        } else {
+            currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+            setTimeout(typePhrase, 10); // Tiempo antes de empezar la siguiente frase (ajustable)
+        }
+    }, 100);
+}
+
+// Comienza el proceso
+typePhrase();
