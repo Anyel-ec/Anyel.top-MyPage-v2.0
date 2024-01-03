e6004449b91e665e305f674cfe286a740210805a
anyel-ec <paulangel826@gmail.com>
2024-01-02 19:56:47 -0500

    Delete images, dark mode improvement

--- assets/js/switch.js
+++ assets/js/switch.js
@@ -1,4 +1,7 @@
-var elements = ["hero-fullscreen"];
+var elements = ["hero-fullscreen", "header", "featured-services", "fs-1",
+    "fs-2", "fs-3", "fs-4", "logos", "certificaciones", "c-1", "c-2", "c-3", "c-4", "c-5", "c-6",
+    "cta", "services", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "main", "s8", "faqlist", "f1", "f2", 
+    "f3", "f4", "f5", "h1", "footer", "about_me", "txt_s1"];
 var darkModeBtn = document.getElementById("dark-mode-btn");
 
 function toggleDarkMode() {
@@ -12,7 +15,7 @@ function toggleDarkMode() {
         element.classList.toggle("bg-dark");
         element.classList.toggle("text-light");
     });
-    
+
 }
 
 // Aplicar el tema oscuro directamente al elemento "hero" al cargar la página
@@ -21,4 +24,5 @@ if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     toggleDarkMode(); // Actualizar automáticamente el icono del botón
 } else {
     document.body.classList.add('light-mode');
+    
 }
