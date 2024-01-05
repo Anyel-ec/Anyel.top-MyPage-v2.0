80c6e84f7252656268a248ac8a6b83adc6a3b4b5
anyel-ec <paulangel826@gmail.com>
2024-01-04 19:37:36 -0500

    Update function to handle edge cases
    This commit updates the function to handle edge cases, as shown in the git diff.

---
+++ seo.php
@@ -0,0 +1,22 @@
+
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description"
+        content="Ingeniero en Tecnologías de la Información, con certificaciones profesionales en áreas de Análisis de Datos, Ciberseguridad, Cloud, Full Stack, apasionado por el mundo digital.😊" />
+    <meta name="keywosrds" content="Anyel EC, Angel Patiño, Angel Paul Patiño Diaz, Cyber Dev Matrix">
+    <name="robots" content="max-image-preview:large" />
+    <link rel="canonical" href="https://www.anyel.top/" />
+    <meta name="generator" content="All in One SEO (AIOSEO) 4.2.7.1 " />
+    <meta property="og:locale" content="es_ES" />
+    <meta property="og:locale:alternate" content="en_US" />
+    <meta property="og:site_name" content="Anyel EC, Angel Paul Patiño Diaz, Cyber Dev Matrix" />
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.anyel.top">
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Anyel EC" />
+    <meta name="twitter:description"
+        content="Ingeniero en Tecnologías de la Información, con certificaciones profesionales en áreas de Análisis de Datos, Ciberseguridad, Cloud, Full Stack, apasionado por el mundo digital.😊😊" />
+    <meta name="author" content="Anyel EC, Angel Paul Patiño Diaz" />
+    <meta name="robots" content="index, follow">
+    
+</head>
